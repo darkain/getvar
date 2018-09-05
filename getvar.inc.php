ad9fd695bdd8915daf22be0062d68968c0c7c3f2
Vince <git@darkain.com>
2018-09-05 10:09:32 -0700

    Reverting to a single file (no need for "include" security now)

--- getvar.inc.php
+++ getvar.inc.php
@@ -2,11 +2,26 @@
 
 
 
-require_once(__DIR__.'/defines.inc.php');
+////////////////////////////////////////////////////////////////////////////////
+// ALL SUPPORTED GETVAR FLAGS
+////////////////////////////////////////////////////////////////////////////////
+define('_GETVAR_BASIC',		0 <<  0);
+define('_GETVAR_NOGET',		1 <<  0);
+define('_GETVAR_NOPOST',	1 <<  1);
+define('_GETVAR_HTMLSAFE',	1 <<  3);
+define('_GETVAR_URLSAFE',	1 <<  4);
+define('_GETVAR_NOTRIM',	1 <<  5);
+define('_GETVAR_NODOUBLE',	1 <<  6);
+define('_GETVAR_UNICODE',	1 <<  7);
+define('_GETVAR_NULL',		1 <<  8);
+define('_GETVAR_CURRENCY',	1 <<  9);
 
 
 
 
+////////////////////////////////////////////////////////////////////////////////
+// THE GETVAR CLASS ITSELF
+////////////////////////////////////////////////////////////////////////////////
 class getvar implements ArrayAccess {
 
 
